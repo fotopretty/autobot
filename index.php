d7560fbfd02ac9365af9dd52a78afe785bf067f2
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-08 19:35:25 +0700

    db

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
 use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 
-$host = "araiwah.com";
+$host = "43.229.77.78";
 $username = "araiwah_pmt";
 $password = "canon50d";
 mysql_connect($host, $username, $password);
