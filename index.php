50333a9312cb0c60e60356fb16820998551d14ea
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-08 19:41:39 +0700

    db

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
 use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 
-$host = "43.229.77.78";
+$host = "araiwah.com";
 $username = "araiwah_pmt";
 $password = "canon50d";
 $objConnect  = mysql_connect($host, $username, $password);
