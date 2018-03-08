5216d25c93f62ed566ea9692ffd5564bc72bd755
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-08 21:57:15 +0700

    db

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
             $textMsg = $event['message']['text'];
             $userProfile = $bot->getProfile($userId);
             $userData = $userProfile->getJSONDecodedBody();
-            $userDisplayName = $userData('displaName');
+            $userDisplayName = $userData('displayName');
 
 
             // Sticker
